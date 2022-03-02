2a8023b1204cf2a6b8a37601df655b1fe4b45441
Robe <robertruben98@gmail.com>
2022-03-02 13:18:08 -0500

    Agregando el route index_name. Nuevo placeholder

--- app.py
+++ app.py
@@ -2,7 +2,10 @@ from flask import Flask
 
 app = Flask(__name__)
 
-
 @app.route('/')
 def index():
-    return '<h1>Hello!</h1>'
+    return '<h1>Hello stranger</h1>'
+
+@app.route('/<name>')
+def index_name(name):
+    return f'<h1>Hello {name}!</h1>'
