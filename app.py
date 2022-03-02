509deb8c336d490f0e6611400308a47bd7dec1ce
Robe <robertruben98@gmail.com>
2022-03-02 13:55:49 -0500

    Agregamos un default name

--- app.py
+++ app.py
@@ -8,13 +8,15 @@ def index():
     return '<h1>Hello stranger</h1>'
 
 
+@app.route('/home', methods=['GET'], defaults={'name': 'Default'})
 @app.route('/home/<name>', methods=['GET'])
 def home(name):
     return f'<h1>Hello {name},you are on the home page!'
 
+
 @app.route('/json')
 def json():
-    return jsonify({'key': 'value', 'key2': [1,2,3,4]})
+    return jsonify({'key': 'value', 'key2': [1, 2, 3, 4]})
 
 
 if __name__ == '__main__':
