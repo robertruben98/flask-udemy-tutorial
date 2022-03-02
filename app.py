21d0dfcad9765f4ea26d9a76fb61234ceb09e302
Robe <robertruben98@gmail.com>
2022-03-02 13:34:07 -0500

    Agregamos route '/home'

--- app.py
+++ app.py
@@ -2,13 +2,21 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def index():
     return '<h1>Hello stranger</h1>'
 
+
+@app.route('/home')
+def home():
+    return '<h1>You are on the home page!'
+
+
 @app.route('/<name>')
 def index_name(name):
     return f'<h1>Hello {name}!</h1>'
+
 
 if __name__ == '__main__':
     app.run()
